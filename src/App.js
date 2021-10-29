24ad4b640b3b840e77b9e17c5ecd24cef1a83216
fraz <01-1314181-021@student.bahria.edu.pk>
2021-10-29 15:21:54 +0500

    pinata on react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import { create } from 'ipfs-http-client'
+const pinataSDK = require("@pinata/sdk");
+const client = create('https://ipfs.infura.io:5001/api/v0')
+
+// I know keys are here
+const pinata = pinataSDK(
+  "1bbb9d9e051a0ff19ade",
+  "4b6fcef506f1387910ad6c6a023dfd5d014461313673e6ab494cd91c2157fe2b"
+);
 
 function App() {
+  const [fileUrl, updateFileUrl] = useState(``)
+  const [metadata, setMetadata] = useState("")
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const img = `https://ipfs.infura.io/ipfs/${added.path}`
+      updateFileUrl(img);
+
+      const body = {
+        image: img
+    };
+    const options = {
+        pinataMetadata: {
+            name: "Crease Token",
+            keyvalues: {
+                customKey: 'Crease Metadata',
+                customKey2: Math.random()
+            }
+        },
+        pinataOptions: {
+            cidVersion: 0
+        }
+    };
+    pinata.pinJSONToIPFS(body, options).then((result) => {
+      var metadataURI  = "https://gateway.pinata.cloud/ipfs/" + result.IpfsHash;
+
+    }).catch((err) => {
+        console.log(err);
+    });
+
+
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS Example</h1>
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      {
+        fileUrl && (
+          <img src={fileUrl} width="600px" />
+        )
+      }
+      <p>{fileUrl}</p>
     </div>
   );
 }
 
-export default App;
+export default App
